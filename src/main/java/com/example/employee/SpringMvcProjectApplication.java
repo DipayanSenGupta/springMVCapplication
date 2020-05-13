1f479db03ad42c3543e55d74482db2c00e2ee6f6
dip.himu <dip.himu@gmail.com>
2020-05-13 20:30:48 +0600

    Add main class

---
+++ src/main/java/com/example/employee/SpringMvcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee;
+
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class SpringMvcProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringMvcProjectApplication.class, args);
+	}
+
+}
