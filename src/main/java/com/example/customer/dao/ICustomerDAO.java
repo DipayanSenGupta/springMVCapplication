2ba26426401691208731ae11acdef3d00d2a5f1a
dip.himu <dip.himu@gmail.com>
2020-05-15 10:32:14 +0600

    Fix findByAccountNum

--- src/main/java/com/example/customer/dao/ICustomerDAO.java
+++ src/main/java/com/example/customer/dao/ICustomerDAO.java
@@ -1,6 +1,5 @@
 package com.example.customer.dao;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +9,6 @@ import com.example.customer.model.Customer;
 @Repository
 public interface ICustomerDAO extends JpaRepository<Customer, Integer> {
 
-	List<Customer> findByAccountNum(String customAccountNum);
+Customer findByAccountNum(String accountNum);
+
 }
